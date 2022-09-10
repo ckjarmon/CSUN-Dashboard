4c0a20663b48757cf71947f389a47c14ec67aa81
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-09-10 04:48:21 -0700

    ...

--- code-assets/scripts/emails_names_mysql.py
+++ code-assets/scripts/emails_names_mysql.py
@@ -1,4 +1,3 @@
-
 import re
 import json
 import urllib3
@@ -7,12 +6,7 @@ import mysql.connector
 from mysql.connector import errorcode
 
 
-
 class_codes = ["AE","AM","AAS","ACCT","AFRS","AIS","ANTH","ARAB","ARMN","ART","ASTR","ATHL","BANA","BIOL","BLAW","BUS","CE","CADV","CAS","CCE","CD","CECS","CHS","CHEM","CHIN","CIT","CJS","CLAS","CM","COMP","COMS","CTVA","DEAF","EED","ECE","ECON","EDUC","ELPS","ENGL","EOH","EPC","FCS","FIN","FLIT","FREN","GBUS","GEOG","GEOL","GWS","HEBR","HIST","HSCI","HUM","INDS","IS","ITAL","JS","JAPN","JOUR","KIN","KNFC","KOR","LING","LRS","ME","MATH","MCOM","MGT","MKT","MSE","MUS","NURS","PERS","PHIL","PHSC","PHYS","POLS","PSY","PT","QS","RS","RE","RTM","RUSS","SED","SCI","SCM","SOC","SOM","SPAN","SPED","SUST","SWRK","TH","UNIV","URBS"]
-
-
-
-
 
 try:
     rootConnection = mysql.connector.connect(
