62679b66701c798453ac8729c41fd0a9cd66dad3
MikeB1124 <balianmichael@gmail.com>
2022-09-10 14:08:08 -0700

    landing page

---
+++ code-assets/frontend/csun-dash/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from './HomePage'
+
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
---
+++ code-assets/frontend/csun-dash/src/components/Classes.js
@@ -0,0 +1,10 @@
+
+function Classes(props){
+    return(
+        <div id="classes" style={{height:"100vh"}}>
+
+        </div>
+    )
+}
+
+export default Classes;
