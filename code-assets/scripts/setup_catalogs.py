a1111b9a9702a613cda4120588bc68028cfbb069
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-09-12 02:34:15 -0700

    ...

--- code-assets/scripts/setup_catalogs.py
+++ code-assets/scripts/setup_catalogs.py
@@ -6,6 +6,7 @@ import json
 
 class_codes = ["AE","AM","AAS","ACCT","AFRS","AIS","ANTH","ARAB","ARMN","ART","ASTR","ATHL","BANA","BIOL","BLAW","BUS","CE","CADV","CAS","CCE","CD","CECS","CHS","CHEM","CHIN","CIT","CJS","CLAS","CM","COMP","COMS","CTVA","DEAF","EED","ECE","ECON","EDUC","ELPS","ENGL","EOH","EPC","FCS","FIN","FLIT","FREN","GBUS","GEOG","GEOL","GWS","HEBR","HIST","HSCI","HUM","INDS","IS","ITAL","JS","JAPN","JOUR","KIN","KNFC","KOR","LING","LRS","ME","MATH","MCOM","MGT","MKT","MSE","MUS","NURS","PERS","PHIL","PHSC","PHYS","POLS","PSY","PT","QS","RS","RE","RTM","RUSS","SED","SCI","SCM","SOC","SOM","SPAN","SPED","SUST","SWRK","TH","UNIV","URBS"]
 
+
 sems = ["Fall", "Spring"]
 for code in class_codes:
     tuples = []
@@ -37,7 +38,6 @@ for code in class_codes:
 
 
     catalog_file = open(f"../../code-assets/backend/json_catalog/{code}_catalog.json", "w")
-
     json.dump(json_blobs, catalog_file, indent=4)
 
 
@@ -76,28 +76,33 @@ for code in class_codes:
                     course["description"] = driver.find_element("id", "inset-content").find_element(By.CLASS_NAME, "section-content").text
                 except NoSuchElementException as e:
                     continue
+            new_desc_file = open(f"../../code-assets/backend/json_catalog/{code}_catalog.json", "w")
+            json.dump(classes, new_desc_file, indent=4)
+            new_desc_file.close() 
         except json.JSONDecodeError as jce:
             with open('ERROR_LOG.txt', 'a') as f:
                 f.write(str(jce))
                 f.write("\n")
-                f.write(str(curr_sub))
-                f.write("\n")
+
         
-    new_desc_file = open(f"../../code-assets/backend/json_catalog/{code}_catalog.json", "w")
-    json.dump(classes, new_desc_file, indent=4)
-    new_desc_file.close()    
+           
 
 
 for code in class_codes:
     print(code)
     with open(f"../../code-assets/backend/json_catalog/{code}_catalog.json") as curr_sub:
-        subject_dict = json.load(curr_sub)
-        for course in subject_dict:
-            if course["description"] is None:
-                del course
-        subject_file = open(f"../backend/json_catalog/{code}_catalog.json", "w")
-        json.dump(subject_dict, subject_file, indent=4)
-        subject_file.close()
+        try:
+            subject_dict = json.load(curr_sub)
+            for course in subject_dict:
+                if course["description"] is None:
+                    del course
+            subject_file = open(f"../backend/json_catalog/{code}_catalog.json", "w")
+            json.dump(subject_dict, subject_file, indent=4)
+            subject_file.close()
+        except json.JSONDecodeError as jce:
+            with open('ERROR_LOG.txt', 'a') as f:
+                f.write(str(jce))
+                f.write("\n")
 
 
 
@@ -114,12 +119,16 @@ sorts every course document blob by its catalog number
 for code in class_codes:
     print(code)
     with open(f"../../code-assets/backend/json_catalog/{code}_catalog.json") as curr_sub:
-        subject_dict = json.load(curr_sub)
-        subject_dict.sort(key = lambda item: item.get("catalog_number"))
-        subject_file = open(f"../backend/json_catalog/{code}_catalog.json", "w")
-        json.dump(subject_dict, subject_file, indent=4)
-        subject_file.close()
-            
+        try:
+            subject_dict = json.load(curr_sub)
+            subject_dict.sort(key = lambda item: item.get("catalog_number"))
+            subject_file = open(f"../backend/json_catalog/{code}_catalog.json", "w")
+            json.dump(subject_dict, subject_file, indent=4)
+            subject_file.close()
+        except json.JSONDecodeError as jce:
+            with open('ERROR_LOG.txt', 'a') as f:
+                f.write(str(jce))
+                f.write("\n")      
         
 
 """-------------------------------------------------------------------------'''
@@ -195,6 +204,4 @@ for code in class_codes:
         except json.JSONDecodeError as jce:
             with open('ERROR_LOG.txt', 'a') as f:
                 f.write(str(jce))
-                f.write("\n")
-                f.write(str(curr_sub)) 
                 f.write("\n")
