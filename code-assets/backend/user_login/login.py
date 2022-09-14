a269e7505b58d8cdf03f99f783c05e329f5659cb
Nima Shafie <nimzshafie@gmail.com>
2022-09-13 17:52:32 -0700

    starting to mess with user login with flask

--- code-assets/backend/user_login/login.py
+++ code-assets/backend/user_login/login.py
@@ -1,12 +1,23 @@
 from flask import Flask, render_template
 
-# render template 
+# render template
+# return render_template("index.html")
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def login():
     return render_template("index.html")
+
+print("Passing the numbers: 1,2,3")
+answer = sum([1,2,3])
+print("What is the result of 1+2+3?: %d" %answer)
+
+def sum(*args):
+    finalResult = 0
+    for number in args:
+        finalResult += number
+    return finalResult
 
 if __name__ == '__main__':
     app.run(debug=True)
