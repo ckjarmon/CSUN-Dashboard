a909a91562a05b935da7ad5106def55a101589fc
Nima Shafie <nimzshafie@gmail.com>
2022-09-17 11:00:39 -0700

    working on user account

--- code-assets/backend/user_login/login.py
+++ code-assets/backend/user_login/login.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
+# render template 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask!"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
