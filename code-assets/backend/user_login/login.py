3a15ba029c1b93416f6b791b6d0ef67458595826
Nima Shafie <nimzshafie@gmail.com>
2022-09-27 15:03:07 -0700

    attempting to fix error

--- code-assets/backend/user_login/login.py
+++ code-assets/backend/user_login/login.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 
-# render template 
+# render template
+# return render_template("index.html")
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def login():
     return render_template("index.html")
 
 if __name__ == '__main__':
