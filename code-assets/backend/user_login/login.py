68fa2dd672d414eb40a92440ac1dc8be8c975bc4
Nima Shafie <nimzshafie@gmail.com>
2022-09-27 15:44:28 -0700

    testing args and kwargs

--- code-assets/backend/user_login/login.py
+++ code-assets/backend/user_login/login.py
@@ -9,5 +9,15 @@ app = Flask(__name__)
 def login():
     return render_template("index.html")
 
+print("Passing the numbers: 1,2,3")
+answer = sum([1,2,3])
+print("What is the result of 1+2+3?: %d" %answer)
+
+def sum(*args):
+    finalResult = 0
+    for number in args:
+        finalResult += number
+    return finalResult
+
 if __name__ == '__main__':
     app.run(debug=True)
