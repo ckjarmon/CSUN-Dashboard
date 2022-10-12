b0896b6eb7ab95049eafd2153f23b5f4f40b644b
Nima Shafie <nimzshafie@gmail.com>
2022-10-11 18:57:40 -0700

    working on login test

--- test/backend/user_login/login_test.py
+++ test/backend/user_login/login_test.py
@@ -1,12 +1,12 @@
+from login import sum_it
 from unittest import TestCase
 import sys
 sys.path.append(".\\code-assets\\backend\\user_login")
-from login import sum_it
 
 """
 Tests functionality from login.py
 """
 
-def test_sum_empty() -> None:
-     sum()
 
+def test_sum_empty() -> None:
+    sum()
