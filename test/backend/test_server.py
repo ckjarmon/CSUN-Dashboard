3b11bc2ee3101c132c0967a74b6f17a58405a8b0
Nima Shafie <nimzshafie@gmail.com>
2022-11-22 17:15:19 -0800

    working on server unit testing

--- test/backend/test_server.py
+++ test/backend/test_server.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 import sys
 import path
 import pytest
+import warnings
 
 # determines os and finds files to test
 ################################################################
@@ -18,18 +19,38 @@ print("Printing the folder parent here: %s" %currDirectory)
 ################################################################
 
 # search for correct dir and file and import the methods to be tested
-from backend.server import name_normalize
+from backend.server import name_normalize, prof_name
 
+################################################################
 # test the name_normalization function
 def test_name_normalize_common_returns_regular():
      string_to_pass = "bOb"
      assert name_normalize(string_to_pass) == "Bob"
+     string_to_pass = "bob"
+     assert name_normalize(string_to_pass) == "Bob"
 
+# name_normalize behavior should throw a type error to be correct
+def test_name_normalize_int_returns_error():
+     int_to_pass = 123
+     with pytest.raises(TypeError):
+          name_normalize(int_to_pass)
 
+# def test_name_normalize_empty_returns_null():
+#      empty_passed = None
+#      assert name_normalize(empty_passed) == ""
 
+################################################################
 
-# def func(x):
-#      return x + 1
+def test_prof_name_returns_correct_name():
+     example_prof = "John Noga"
+     assert prof_name(subject="comp", prof_email="john.noga@csun.edu") == example_prof
 
-# def test_answer():
-#      assert func(3) == 5
+@pytest.mark.problem_def
+# proves that def prof_name(**kwargs) needs to handle invalid input
+# when supplied invalid information, the function returns an index error
+def test_prof_name_incorrect_returns_error():
+     with pytest.raises(IndexError):
+          warnings.warn(UserWarning("prof_name located in code-assets/backend/server.py \n"
+          "Throws an IndexError exception if passed invalid subject/prof_email"))
+          assert prof_name(subject="null", prof_email="unknown@csun.edu")
+
