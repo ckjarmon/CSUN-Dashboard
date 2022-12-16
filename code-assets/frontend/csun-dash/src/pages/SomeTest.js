6ee6a59738f09cc775d4709babf33cefaae74e74
Nima Shafie <nimzshafie@gmail.com>
2022-12-15 16:40:20 -0800

    test examples, shown, starting more tests

---
+++ code-assets/frontend/csun-dash/src/pages/SomeTest.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function TryThis() {
+    return (6)
+}
+
+function TryThat() {
+    return (7)
+}
+
+export {TryThis, TryThat}
---
+++ test/frontend/professor.search.test.js
@@ -0,0 +1,20 @@
+import {ProfessorsSearchPage} from '../../code-assets/frontend/csun-dash/src/pages/ProfessorSearchPage'
+
+test('returns the correct section number based on class (16615) for Comp 589', () => {
+  expect(ProfessorsSearchPage()).toBe(16615);
+});
+
+
+
+
+/*
+Format to write tests
+import {func1, funcN} from '../../code-assets/frontend/csun-dash/src/pages/SomeTest'
+
+test("returns the correct list of professors", () => {
+    expect(TryThis()).toBe(6);
+});
+
+in SomeTest.js we need to export all functions
+export {TryThis, TryThat}
+*/
