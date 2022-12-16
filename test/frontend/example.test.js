d5d7b5a301a7d805a180f4e4a1eecebcb4fd8d40
Nima Shafie <nimzshafie@gmail.com>
2022-12-15 16:40:37 -0800

    test example shown, starting more tests

---
+++ test/frontend/example.test.js
@@ -0,0 +1,27 @@
+// import {TryThis, TryThat} from '../../code-assets/frontend/csun-dash/src/pages/SomeTest'
+
+// test('returns the correct section number based on class (16615) for Comp 589', () => {
+//   expect(ProfessorsSearchPage()).toBe(16615);
+// });
+
+
+// test("returns the correct list of professors", () => {
+//     expect(TryThis()).toBe(6);
+// });
+
+// test("returns the correct list of professors", () => {
+//   expect(TryThat()).toBe(7);
+// });
+
+
+/*
+Format to write tests
+import {func1, funcN} from '../../code-assets/frontend/csun-dash/src/pages/SomeTest'
+
+test("returns the correct list of professors", () => {
+    expect(TryThis()).toBe(6);
+});
+
+in SomeTest.js we need to export all functions
+export {TryThis, TryThat}
+*/
