17f7a3e8eb34df22d96aecd4730c1c57156d32bf
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2023-01-03 18:58:42 -0800

    changed var names in playground for parse

--- code-assets/backend/scripts/kyeou_playground.py
+++ code-assets/backend/scripts/kyeou_playground.py
@@ -11,7 +11,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-
+import mariadb
 
 
 class_codes = ["AE","AM","AAS","ACCT","AFRS","AIS","ANTH","ARAB","ARMN","ART","ASTR","ATHL","BANA","BIOL","BLAW","BUS","CE","CADV","CAS","CCE","CD","CECS","CHS","CHEM","CHIN","CIT","CJS","CLAS","CM","COMP","COMS","CTVA","DEAF","EED","ECE","ECON","EDUC","ELPS","ENGL","EOH","EPC","FCS","FIN","FLIT","FREN","GBUS","GEOG","GEOL","GWS","HEBR","HIST","HSCI","HUM","INDS","IS","ITAL","JS","JAPN","JOUR","KIN","KNFC","KOR","LING","LRS","ME","MATH","MCOM","MGT","MKT","MSE","MUS","NURS","PERS","PHIL","PHSC","PHYS","POLS","PSY","PT","QS","RS","RE","RTM","RUSS","SED","SCI","SCM","SOC","SOM","SPAN","SPED","SUST","SWRK","TH","UNIV","URBS"]
@@ -586,54 +586,50 @@ def parse_dead(prereq):
            
     
 # do not call initial parse call with empty string
-# all initial calls should start with _res = "Take "
-def parse(_sp, _res):
-    sp = _sp[1:-1]
-    print(sp)
-    p_stack = []
-    c = 0
-    res = _res
+# all initial calls should start with _start = "Take "
+def parse(_parseable, _start):
+    unparsed, parsed_ret = _parseable[1:-1], _start
+    c_iterate, parse_stack = 0, []
     
-    
-    while c < len(sp):
-        match sp[c]:
+    while c_iterate < len(unparsed):
+        match unparsed[c_iterate]:
             case '{':
-                if sp[c+1] == '{':
-                    bb = sp[c:].index('}}')
-                    res += parse(sp[c:][0:bb+3], "")
-                    c += bb + 1
+                if unparsed[c_iterate+1] == '{':
+                    sub_unparsed_end = unparsed[c_iterate:].index('}}')
+                    parsed_ret += parse(unparsed[c_iterate:][0:sub_unparsed_end+3], "")
+                    c_iterate += sub_unparsed_end + 1
                 else:
-                    p_stack.append(sp[c])
+                    parse_stack.append(unparsed[c_iterate])
             case '}':
                 try:
-                    p_stack.pop()
+                    parse_stack.pop()
                 except IndexError:
                     pass
             case '&':
-                if _res == "":
-                    res += "AND Take "
+                if _start == "":
+                    parsed_ret += "AND Take "
                 else:
-                    res += "\nAND\nTake "
-                c += 1
+                    parsed_ret += "<br>AND<br>Take "
+                c_iterate += 1
             case '|':
-                if _res == "":
-                    res += "OR Take"
+                if _start == "":
+                    parsed_ret += "OR Take"
                 else:     
-                    res += "\nor\nTake"
-                c += 1               
+                    parsed_ret += "<br>or<br>Take"
+                c_iterate += 1               
             case _:
-                res += sp[c]
-        c += 1
+                parsed_ret += unparsed[c_iterate]
+        c_iterate += 1
     # cba programming the logic to avoid these from happening so just do replace calls lol
-    res = res.replace('Take Obtain', 'Obtain')
-    res = res.replace('Take Earn', 'Earn')
-    res = res.replace('Take Permission', 'Permission')
-    res = res.replace('Take None', 'None')
-    res = res.replace('Take Be', 'Be')
-    res = res.replace('\n ', '\n').replace('  ', ' ')
-    return repr(res) # .replace('or a passing score', 'or Earn a passing score')
+    parsed_ret = parsed_ret.replace('Take Obtain', 'Obtain')
+    parsed_ret = parsed_ret.replace('Take Earn', 'Earn')
+    parsed_ret = parsed_ret.replace('Take Permission', 'Permission')
+    parsed_ret = parsed_ret.replace('Take None', 'None')
+    parsed_ret = parsed_ret.replace('Take Be', 'Be')
+    parsed_ret = parsed_ret.replace('\n ', '\n').replace('  ', ' ')
+    return repr(parsed_ret).replace('\'', '') # .replace('or a passing score', 'or Earn a passing score')
     
-import mariadb
+
 if __name__ == "__main__":
     try:
         rootConnection = mariadb.connect(
