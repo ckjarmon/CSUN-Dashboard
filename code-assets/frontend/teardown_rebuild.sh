f714e87e3a5ee16c9e88905a166d11c216a0995e
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2023-02-24 23:23:10 -0800

    im tired of typing these in manually

---
+++ code-assets/frontend/teardown_rebuild.sh
@@ -0,0 +1,6 @@
+cd csun-dash
+npm run build
+sudo rm -rf /var/www/csundash.kyeou.xyz/html
+sudo mkdir /var/www/csundash.kyeou.xyz/html
+sudo cp -r build/* /var/www/csundash.kyeou.xyz/html/ 
+sudo systemctl restart nginx
