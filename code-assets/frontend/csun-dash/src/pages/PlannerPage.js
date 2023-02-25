0a2715b645d73e82a00deca75ef04abbcb73d576
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2023-02-25 11:11:22 -0800

    added times to conflict logic and changed error messages

--- code-assets/frontend/csun-dash/src/pages/PlannerPage.js
+++ code-assets/frontend/csun-dash/src/pages/PlannerPage.js
@@ -3,33 +3,46 @@ import AddClasses from "../elements/Add_Classes/AddClasses";
 import ClassSelections from "../elements/Class_Selections/ClassSelections";
 import { useState } from 'react'
 
-function PlannerPage({handleAlert}) {
+function PlannerPage({ handleAlert }) {
     const [addedClasses, setAddedClasses] = useState([])
 
     const [addedClassIcon, setAddedClassIcon] = useState({})
 
-    function classAlreadyAdded(event) {
+    function doesClassConflict(event) {
         for (let i = 0; i < addedClasses.length; i++) {
-            if (event.catalog_number === addedClasses[i].catalog_number) {
-                return true
-            }
+
+
+            let new_start = event.start_time.substring(0, event.start_time.length - 1)
+            let new_end = event.end_time.substring(0, event.end_time.length - 1)
+
+            let added_start = addedClasses[i].start_time.substring(0, addedClasses[i].start_time.length - 1)
+            let added_end = addedClasses[i].end_time.substring(0, addedClasses[i].end_time.length - 1)
+
+
+            if (new_start > added_start && new_start < added_end) {return true}
+            if (new_end > added_start && new_end < added_end) {return true}
+
+            if (added_start > new_start && added_start < new_start) {return true}
+            if (added_end > new_start && added_end < new_end) {return true}
+
+            if (event.catalog_number === addedClasses[i].catalog_number) { return true }
         }
         return false
     }
 
     function handleClassAdded(event) {
         if (!addedClassIcon[event.class_number]) {
-            if (!classAlreadyAdded(event)) {
+            if (!doesClassConflict(event)) {
                 setAddedClassIcon({
                     ...addedClassIcon,
                     [event.class_number]: !addedClassIcon[event.class_number]
                 })
                 setAddedClasses(oldArray => [...oldArray, event])
                 console.log(event)
-                handleAlert({severity: "success", message: "Class Has Been Added To Planner"})
+                handleAlert({ severity: "success", message: "Class has been added to your planner." })
             } else {
-                handleAlert({severity: "error", message: "This Class Is Already Added In Your Planner"})
-                console.log("This Class Is Already Added To Your Schedule!")
+                handleAlert({ severity: "error", message: "This class conflicts with current schedule!" })
+                console.log("This class conflicts with current schedule!")
             }
         } else {
 
